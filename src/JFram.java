e2a767abc8dc45462f0d0d4ce56b7f89bc6060ea
Thilaphat <100566477+Thilaphat@users.noreply.github.com>
2022-02-28 20:27:11 +0700

    Library

---
+++ src/JFram.java
@@ -0,0 +1,19 @@
+
+import Project.ConnectionProvider;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author COMCOM
+ */
+class JFram {
+    public static void main(String[] args)
+    {
+        ConnectionProvider.getCon();
+}
+}
